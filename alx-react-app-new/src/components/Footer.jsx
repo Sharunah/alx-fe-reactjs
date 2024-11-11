f35ec80dcb7c067ce38308f532ef7ab69128a2d7
Sharunah <cheptoosharon28@gmail.com>
2024-11-11 22:32:05 +0300

    update files

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Footer = () => {
+    return (
+        <footer>
+        <p  style={{ backgroundColor:'yellow' , color: 'beige'}}>© 2023 City Lovers</p>
+      </footer> 
+    )
+}
+export default Footer 
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const MainContent = () => {
+    return (
+        <main>
+        <p style={{ backgroundColor: 'blue' , color:'green' }}>I love to visit New York, Paris, and Tokyo.</p>
+      </main> 
+    )
+}
+export default MainContent
