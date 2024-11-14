28fc811dac2eb5adef1b3ea57a759ef65ce8d3e6
Sharunah <cheptoosharon28@gmail.com>
2024-11-14 22:39:51 +0300

    add file

---
+++ alx-react-app -props/src/components/video.jsx
@@ -0,0 +1,10 @@
+return (
+    <div>
+      <Thumbnail video={video} />
+      <a href={video.url}>
+        <h3>{video.title}</h3>
+        <p>{video.description}</p>
+      </a>
+      <LikeButton video={video} />
+    </div>
+  );
