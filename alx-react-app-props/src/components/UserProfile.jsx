6ae2fd15e6b5edefc7d01e64e2c2ef2612e98b72
Sharunah <cheptoosharon28@gmail.com>
2024-11-15 22:33:24 +0300

    update UserProfile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,29 @@
-import React from "react";
-const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
-  export default UserProfile
+// import React from "react";
+// const UserProfile = (props) => {
+//     return (
+//       <div>
+//         <h2>{props.name}</h2>
+//         <p>Age: {props.age}</p>
+//         <p>Bio: {props.bio}</p>
+//       </div>
+//     );
+//   };
+//   export default UserProfile
+// src/components/UserProfile.jsx
+import React, { useContext } from 'react';
+import UserContext from '../UserContext';
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Age: {userData.age}</p>
+      <p>City: {userData.city}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
