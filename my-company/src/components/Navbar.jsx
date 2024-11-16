80f80ed7be9894fb2395677a98313f3a6ea878fe
Sharunah <cheptoosharon28@gmail.com>
2024-11-17 00:25:52 +0300

    update file

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,32 +3,27 @@ import { Link } from "react-router-dom";
 function Navbar () {
     
     const styles = {
-        container: {
-            padding: '50px 20px',
-            textAlign: 'center',
-            backgroundColor: '#f1f1f1',
-          },
-          heading: {
-            fontSize: '32px',
-            color: '#333',
-            marginBottom: '20px',
-          },
-          paragraph: {
-            fontSize: '18px',
-            color: '#555',
-            lineHeight: '1.6',
-            marginBottom: '20px',
-          },
-          list: {
-            listStyleType: 'none',
-            padding: 0,
-            fontSize: '18px',
-            color: '#555',
-          },
-          listItem: {
-            marginBottom: '10px',
-          },
-        };
+        navbar: {
+          backgroundColor: '#333',
+          padding: '10px',
+          textAlign: 'center',
+        },
+        navList: {
+          listStyleType: 'none',
+          padding: 0,
+          margin: 0,
+          display: 'flex',
+          justifyContent: 'center',
+        },
+        navItem: {
+          margin: '0 15px',
+        },
+        link: {
+          color: '#fff',
+          textDecoration: 'none',
+          fontSize: '18px',
+        },
+      };
     return (
         <nav style={styles.navbar}>
         <ul style={styles.navList}>
