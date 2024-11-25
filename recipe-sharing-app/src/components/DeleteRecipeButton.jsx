9d2257d26e8e0bd412dd05bcc63578fae9227809
Sharunah <cheptoosharon28@gmail.com>
2024-11-25 17:34:24 +0300

    add use navigate from react dom

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import useRecipeStore from './recipeStore';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const { deleteRecipe } = useRecipeStore();
+  const navigate = useNavigate();
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
+    navigate('/details'); // Replace with the path you want to navigate to
   };
 
   return (
