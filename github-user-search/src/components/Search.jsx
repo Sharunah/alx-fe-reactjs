e82f7796b7ae1bd843117629bfe5481a470e9d1e
Sharunah <cheptoosharon28@gmail.com>
2024-12-14 23:37:08 +0300

    Integrating the GitHub API and Creating a Basic Search Feature

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -17,7 +17,7 @@ const Search = () => {
       const response = await axios.get(`https://api.github.com/users/${username}`);
       setUserData(response.data);
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError["Looks like we cant find the user"];
     } finally {
       setLoading(false);
     }
